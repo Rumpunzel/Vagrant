5b96d531ff0110f4f976d66d926d2cb2e694f2ae
Jamal Bencharki <jb@neckar.it>
2024-02-20 09:00:46 +0100

    fixed dice roll sounds

--- dice/dice_roll_sounds.gd
+++ dice/dice_roll_sounds.gd
@@ -1,8 +1,11 @@
 extends AudioStreamPlayer
 
-@export var stagger_delay_ms := 2.0
+@export var _stagger_delay_msec := 20
+@export var _pitch_randomness_perc := 20
 
-var _time_since_last_sound := 0.0
+var _last_sound_played_at_msec := 0
+
+@onready var _dice_polyphony := get_stream_playback()
 
 func _enter_tree() -> void:
 	Rules.d4.rolled.connect(_on_die_type_rolled)
@@ -11,13 +14,11 @@ func _enter_tree() -> void:
 	Rules.d10.rolled.connect(_on_die_type_rolled)
 	Rules.d12.rolled.connect(_on_die_type_rolled)
 
-func _process(delta: float) -> void:
-	_time_since_last_sound = max(_time_since_last_sound - delta, 0.0)
-
 func _on_die_type_rolled(_die_type: DieType, _result: int, roll_sound: AudioStream) -> void:
 	if roll_sound == null: return
-	var delay := _time_since_last_sound
-	_time_since_last_sound += stagger_delay_ms / 1000.0
-	await get_tree().create_timer(delay).timeout
-	var dice_polyphony := get_stream_playback()
-	dice_polyphony.play_stream(roll_sound)
+	var msec_since_last_sound := Time.get_ticks_msec() - _last_sound_played_at_msec
+	var delay_msec := maxi(_stagger_delay_msec - msec_since_last_sound, 0)
+	_last_sound_played_at_msec = Time.get_ticks_msec() + delay_msec
+	if delay_msec > 0: await get_tree().create_timer(delay_msec / 1000.0).timeout
+	var random_pitch := randf_range(1.0 - _pitch_randomness_perc / 100.0, 1.0 + _pitch_randomness_perc / 100.0)
+	_dice_polyphony.play_stream(roll_sound, 0.0, 0.0, random_pitch)
