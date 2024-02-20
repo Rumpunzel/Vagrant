f4951ffd55f395b9d9db0f2aaf3be86277a2a4e1
Jamal Bencharki <jb@neckar.it>
2024-02-20 11:55:49 +0100

    added colors for character attributes

--- characters/attributes/character_attribute.gd
+++ characters/attributes/character_attribute.gd
@@ -1,7 +1,8 @@
 class_name CharacterAttribute
 extends Resource
 
-@export var descriptor := ""
+@export_placeholder("Descriptor") var descriptor: String
+@export var color: Color
 
 func _to_string() -> String:
 	return descriptor
