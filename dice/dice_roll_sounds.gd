09662ea839d9f076f644434b15000d54435697c7
Jamal Bencharki <jb@neckar.it>
2024-02-21 12:22:01 +0100

    adjusted dice roll sound randomness algorithm

--- dice/dice_roll_sounds.gd
+++ dice/dice_roll_sounds.gd
@@ -1,6 +1,7 @@
 extends AudioStreamPlayer
 
-@export var _stagger_delay_msec := 20
+@export var _delay_window_mean_msec := 100
+@export var _delay_window_deviation_msec := 100
 @export var _pitch_randomness_perc := 20
 
 var _last_sound_played_at_msec := 0
@@ -17,8 +18,10 @@ func _enter_tree() -> void:
 func _on_die_type_rolled(_die_type: DieType, _result: int, roll_sound: AudioStream) -> void:
 	if roll_sound == null: return
 	var msec_since_last_sound := Time.get_ticks_msec() - _last_sound_played_at_msec
-	var delay_msec := maxi(_stagger_delay_msec - msec_since_last_sound, 0)
-	_last_sound_played_at_msec = Time.get_ticks_msec() + delay_msec
-	if delay_msec > 0: await get_tree().create_timer(delay_msec / 1000.0).timeout
+	_last_sound_played_at_msec = Time.get_ticks_msec()
+	if msec_since_last_sound < _delay_window_mean_msec:
+		var random_delay := randfn(_delay_window_mean_msec, _delay_window_deviation_msec)
+		_last_sound_played_at_msec += int(random_delay)
+		await get_tree().create_timer(random_delay / 1000.0).timeout
 	var random_pitch := randf_range(1.0 - _pitch_randomness_perc / 100.0, 1.0 + _pitch_randomness_perc / 100.0)
 	_dice_polyphony.play_stream(roll_sound, 0.0, 0.0, random_pitch)
