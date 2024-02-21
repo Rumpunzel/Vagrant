98ae2c8ace212ceebc575db4a9d0be9936daf5e8
Jamal Bencharki <jb@neckar.it>
2024-02-21 18:41:34 +0100

    added story page thumbnails

--- story/pages/story_page.gd
+++ story/pages/story_page.gd
@@ -2,5 +2,15 @@
 class_name StoryPage
 extends Resource
 
-@export_multiline var description: String
-@export var decisions: Array[StoryDecision]
+@export_multiline var description: String : get = _get_description
+@export var thumbnail: Texture : get = _get_thumbnail
+@export var decisions: Array[StoryDecision] : get = _get_decisions
+
+func _get_description() -> String:
+	return description
+
+func _get_thumbnail() -> Texture:
+	return thumbnail
+
+func _get_decisions() ->  Array[StoryDecision]:
+	return decisions
