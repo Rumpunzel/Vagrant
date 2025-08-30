bedff4f54934fcc0e6e584e10dd8058683efe431
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-08-30 13:24:04 +0200

    Update display_button.gd

--- custom_nodes/control/display_button.gd
+++ custom_nodes/control/display_button.gd
@@ -1,7 +1,11 @@
+@tool
 class_name DisplayButton
 extends Button
 
 signal activation_changed(new_status: bool)
+
+@export var active_mouse_cursor: Control.CursorShape = Control.CURSOR_POINTING_HAND
+@export var inactive_mouse_cursor: Control.CursorShape = Control.CURSOR_ARROW
 
 var active: bool = true :
 	set(new_status):
@@ -9,4 +13,5 @@ var active: bool = true :
 		active = new_status
 		button_mask = MOUSE_BUTTON_MASK_LEFT if active else 0
 		focus_mode = Control.FOCUS_ALL if active else Control.FOCUS_NONE
+		mouse_default_cursor_shape = active_mouse_cursor if active else inactive_mouse_cursor
 		activation_changed.emit(active)
