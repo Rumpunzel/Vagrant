b4dfc4fd790c9927e2f641091a74900e695cbfde
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-11-08 20:19:21 +0100

    added hover scroll container

---
+++ custom_nodes/control/hover_scroll_container.gd
@@ -0,0 +1,19 @@
+class_name HoverScrollContainer
+extends ScrollContainer
+
+@onready var _default_horizontal_scroll_mode: ScrollMode = horizontal_scroll_mode
+@onready var _default_vertical_scroll_mode: ScrollMode = vertical_scroll_mode
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_entered() -> void:
+	if _default_horizontal_scroll_mode != ScrollContainer.SCROLL_MODE_DISABLED:
+		horizontal_scroll_mode = ScrollContainer.SCROLL_MODE_RESERVE
+	if _default_vertical_scroll_mode != SCROLL_MODE_DISABLED:
+		vertical_scroll_mode = ScrollContainer.SCROLL_MODE_RESERVE
+
+func _on_mouse_exited() -> void:
+	horizontal_scroll_mode = _default_horizontal_scroll_mode
+	vertical_scroll_mode = _default_vertical_scroll_mode
