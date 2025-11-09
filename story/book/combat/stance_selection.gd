e87c38442c9d1a5001b4acc0796bab2f2cc19057
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-11-09 14:16:49 +0100

    started combat entry implementation

---
+++ story/book/combat/stance_selection.gd
@@ -0,0 +1,52 @@
+@tool
+class_name StanceSelection
+extends PanelContainer
+
+signal character_attribute_selected(character_attribute: CharacterAttribute)
+signal character_attribute_deselected(character_attribute: CharacterAttribute)
+
+@export_group("Configuration")
+@export var _button: DisplayButton
+@export var _icon: TextureRect
+@export var _descriptor: Label
+@export var _details: RichTextLabel
+@export var _color: ColorRect
+@export var _score: RichTextLabel
+
+var attribute: CharacterAttribute :
+	set(new_attribute):
+		assert(new_attribute)
+		assert(new_attribute != attribute)
+		attribute = new_attribute
+		_icon.texture = attribute.icon
+		_descriptor.text = attribute.descriptor
+		_details.text = attribute.details
+		_color.color = attribute.color
+		tooltip_text = attribute.details
+
+var score: BaseAttributeScore :
+	set(new_score):
+		score = new_score
+		update()
+
+var modifiers: Array[AttributeScore.Modifier] = [] :
+	set(new_modifiers):
+		modifiers = new_modifiers
+		update()
+
+func setup(radio_button_group: ButtonGroup) -> void:
+	_button.button_group = radio_button_group
+
+func update() -> void:
+	var attribute_score: AttributeScore = _get_attribute_score()
+	_score.text = "%s = %d" % [attribute_score.get_details(), attribute_score.get_score()]
+
+func disable() -> void:
+	_button.disable()
+
+func _get_attribute_score() -> AttributeScore:
+	return AttributeScore.create_with_modifiers(attribute, score, modifiers)
+
+func _on_button_toggled(toggled_on: bool) -> void:
+	if toggled_on: character_attribute_selected.emit(attribute)
+	else: character_attribute_deselected.emit(attribute)
---
+++ story/book/combat/stance_selection_buttons.gd
@@ -0,0 +1,70 @@
+@tool
+class_name StanceSelectionButtons
+extends PanelContainer
+
+enum Direction {
+	Horizontal,
+	Vertical,
+}
+
+@export_range(0.0, 256.0, 1.0, "exp", "suffix:px") var _button_size: float = 64.0
+@export var _direction: Direction = Direction.Horizontal :
+	set(new_direction):
+		_direction = new_direction
+		if _buttons:
+			_stance_selection_buttons_parent.remove_child(_buttons)
+			_buttons.queue_free()
+		match _direction:
+			Direction.Horizontal: _buttons = HBoxContainer.new()
+			Direction.Vertical: _buttons = VBoxContainer.new()
+			_: assert(false, "Does not exist")
+		_buttons.add_theme_constant_override("separation", 16)
+		_stance_selection_buttons_parent.add_child(_buttons)
+
+@export_group("Configuration")
+@export var _stance_selection_buttons_parent: Container
+@export var _stance_selection_button: PackedScene
+
+var _buttons: BoxContainer
+var _button: Dictionary[CharacterAttribute, StanceSelection] = {}
+
+func _ready() -> void:
+	setup_character_attributes(Rules.ATTRIBUTES)
+
+func setup_character_attributes(character_attributes: Array[CharacterAttribute]) -> void:
+	_clear()
+	var radio_button_group: ButtonGroup = ButtonGroup.new()
+	for character_attribute: CharacterAttribute in character_attributes:
+		add_button(character_attribute, radio_button_group, _button_size)
+
+func disable_buttons() -> void:
+	for button: StanceSelection in _get_buttons(): button.disable()
+
+func add_button(character_attribute: CharacterAttribute, radio_button_group: ButtonGroup, button_size: float) -> void:
+	var button: StanceSelection = _stance_selection_button.instantiate()
+	button.attribute = character_attribute
+	button.setup(radio_button_group)
+	button.custom_minimum_size = Vector2(button_size, button_size)
+	match _direction:
+		Direction.Horizontal: button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		Direction.Vertical: button.size_flags_vertical = Control.SIZE_EXPAND_FILL
+		_: assert(false, "Does not exist")
+	_button[character_attribute] = button
+	button.character_attribute_selected.connect(_on_character_attribute_selected)
+	_buttons.add_child(button)
+
+func _clear() -> void:
+	if not _buttons:
+		_direction = _direction
+		return
+	for child: Node in _buttons.get_children():
+		_buttons.remove_child(child)
+		child.queue_free()
+
+func _get_buttons() -> Array[StanceSelection]:
+	var buttons: Array[StanceSelection] = []
+	buttons.assign(_buttons.get_children())
+	return buttons
+
+func _on_character_attribute_selected(character_attribute: CharacterAttribute) -> void:
+	pass
