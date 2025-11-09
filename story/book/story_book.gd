19973eec8f49891136e19bdfb0a85dd2293a6592
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-11-09 10:46:19 +0100

    layout improvement

--- story/book/story_book.gd
+++ story/book/story_book.gd
@@ -27,10 +27,14 @@ func _exit_tree() -> void:
 	_story.page_entered.disconnect(_on_page_entered)
 
 func _flip_page() -> void:
+	if _current_story_page_entry: _current_story_page_entry.item_rect_changed.disconnect(_on_current_story_page_entry_item_rect_changed)
 	_story_pages.add_child(_current_story_page_entry)
 	_story_pages.move_child(_current_story_page_entry, 0)
 	_current_story_page_entry.enter_page()
 	page_entered.emit(_current_story_page_entry.story_page)
+	await get_tree().process_frame
+	_on_current_story_page_entry_item_rect_changed()
+	_current_story_page_entry.item_rect_changed.connect(_on_current_story_page_entry_item_rect_changed)
 
 func _on_page_entered(story_page: StoryPage) -> void:
 	var previous_page: StoryPageEntry = _current_story_page_entry
@@ -44,3 +48,6 @@ func _on_page_entered(story_page: StoryPage) -> void:
 
 func _on_page_turn_delay_timeout() -> void:
 	_flip_page()
+
+func _on_current_story_page_entry_item_rect_changed() -> void:
+	custom_minimum_size.y = _current_story_page_entry.get_rect().size.y
