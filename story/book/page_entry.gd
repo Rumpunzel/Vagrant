3f2d49966fb89288e355013ee0ab187161720b91
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-11-09 14:30:15 +0100

    factored page entry out of story page entry

---
+++ story/book/page_entry.gd
@@ -0,0 +1,80 @@
+@tool
+class_name PageEntry
+extends PanelContainer
+
+enum State {
+	PAST = -1,
+	PRESENT,
+}
+
+@export var story_page: StoryPage
+@export var state: State = State.PRESENT : set = _set_state
+
+@export_range(0.0, 1.0) var _fade_in_duration: float = 0.1
+
+@export_range(0.0, 3.0) var _fade_out_duration: float = 1.0
+@export_range(0.0, 1.0) var _fade_out_delay: float = 0.5
+@export var _past_modulate: Color = Color(1.0, 1.0, 1.0, 0.25)
+
+@export_group("Configuration")
+@export var _background: BackgroundRect
+@export var _body_container: Container
+@export var _title: TypingLabel
+@export var _description: TypingLabel
+
+var _story: Story
+var _characters: Characters
+
+func setup_page(story: Story, characters: Characters, new_story_page: StoryPage) -> void:
+	_story = story
+	_characters = characters
+	story_page = new_story_page
+	var background: Texture2D = story_page.get_background(_story)
+	_background.texture = background
+	if background:
+		_background.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+		_background.show_behind_parent = false
+	else:
+		_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+		_background.show_behind_parent = true
+
+func enter_page() -> void:
+	var title: String = story_page.get_page_title(_story)
+	if not title.is_empty(): _title.type_text(title)
+	else: _title.visible = false
+	_description.type_text(story_page.get_description(_story))
+
+func _get_fade_out_delay() -> float:
+	return _fade_out_delay
+
+func _set_state(new_state: State) -> void:
+		state = new_state
+		match state:
+			State.PAST:
+				_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+				if not _background.texture:
+					var self_tween: Tween = get_tree().create_tween()
+					self_tween.tween_property(self, "self_modulate", Color.TRANSPARENT, _fade_out_duration).set_delay(_get_fade_out_delay())
+					_background.texture = story_page.get_area_background()
+					_background.fade_in()
+				var tween: Tween = get_tree().create_tween()
+				tween.tween_property(_body_container, "modulate", _past_modulate, _fade_out_duration).set_delay(_get_fade_out_delay())
+				await tween.finished
+				mouse_entered.connect(_on_mouse_entered)
+				mouse_exited.connect(_on_mouse_exited)
+			State.PRESENT:
+				self_modulate = Color.TRANSPARENT if _background.texture else Color.WHITE
+			_: assert(false, "StoryPageEntry.State %s is not supported!" % state)
+
+func _on_mouse_entered() -> void:
+	assert(state == State.PAST)
+	if story_page.get_background(_story): _background.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(_body_container, "modulate", Color.WHITE, _fade_in_duration)
+
+func _on_mouse_exited() -> void:
+	assert(state == State.PAST)
+	if get_global_rect().has_point(get_viewport().get_mouse_position()): return
+	_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(_body_container, "modulate", _past_modulate, _fade_in_duration)
--- story/book/story_page_entry.gd
+++ story/book/story_page_entry.gd
@@ -1,55 +1,16 @@
 @tool
 class_name StoryPageEntry
-extends PanelContainer
+extends PageEntry
 
-enum State {
-	PAST = -1,
-	PRESENT,
-}
-
-@export var story_page: StoryPage
-@export var state: State = State.PRESENT :
-	set(new_state):
-		state = new_state
-		match state:
-			State.PAST:
-				for dialog_button: DialogButton in _choices.get_children(): dialog_button.active = false
-				_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-				if not _background.texture:
-					var self_tween: Tween = get_tree().create_tween()
-					self_tween.tween_property(self, "self_modulate", Color.TRANSPARENT, _fade_out_duration).set_delay(_dice_fade_out_delay if is_dice_page() else _fade_out_delay)
-					_background.texture = story_page.get_area_background()
-					_background.fade_in()
-				var tween: Tween = get_tree().create_tween()
-				tween.tween_property(_body_container, "modulate", _past_modulate, _fade_out_duration).set_delay(_dice_fade_out_delay if is_dice_page() else _fade_out_delay)
-				await tween.finished
-				mouse_entered.connect(_on_mouse_entered)
-				mouse_exited.connect(_on_mouse_exited)
-			State.PRESENT:
-				for dialog_button: DialogButton in _choices.get_children(): dialog_button.active = true
-				self_modulate = Color.TRANSPARENT if _background.texture else Color.WHITE
-			_: assert(false, "StoryPageEntry.State %s is not supported!" % state)
-
-@export_range(0.0, 1.0) var _fade_in_duration: float = 0.1
 @export_range(0.0, 1.0) var _dialog_options_fade_in_delay: float = 0.1
 
-@export_range(0.0, 3.0) var _fade_out_duration: float = 1.0
-@export_range(0.0, 1.0) var _fade_out_delay: float = 0.5
 @export_range(0.0, 5.0) var _dice_fade_out_delay: float = 3.0
-@export var _past_modulate: Color = Color(1.0, 1.0, 1.0, 0.25)
 
 @export_group("Configuration")
-@export var _background: BackgroundRect
-@export var _body_container: Container
-@export var _title: TypingLabel
-@export var _description: TypingLabel
 @export var _choices: Container
 @export var _breath_dice_selection: BreathDiceSelection
 @export var _breath_dice_selection_collapsible_container: CollapsibleContainer
 @export var _dialog_button: PackedScene
-
-var _story: Story
-var _characters: Characters
 
 var _selected_story_decision: StoryDecision
 var _save_request: SaveRequest :
@@ -61,24 +22,11 @@ var _save_request: SaveRequest :
 var _save_result: SaveResult
 
 func setup_page(story: Story, characters: Characters, new_story_page: StoryPage) -> void:
-	_story = story
-	_characters = characters
-	story_page = new_story_page
-	var background: Texture2D = story_page.get_background(_story)
-	_background.texture = background
-	if background:
-		_background.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
-		_background.show_behind_parent = false
-	else:
-		_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-		_background.show_behind_parent = true
+	super.setup_page(story, characters, new_story_page)
 	_update_decisions(story_page.get_decisions(_story))
 
 func enter_page() -> void:
-	var title: String = story_page.get_page_title(_story)
-	if not title.is_empty(): _title.type_text(title)
-	else: _title.visible = false
-	_description.type_text(story_page.get_description(_story))
+	super.enter_page()
 	_story.decision_made.connect(_on_decision_made)
 
 func _exit_tree() -> void:
@@ -104,6 +52,18 @@ func _create_dialog_button(story_decision: StoryDecision) -> DialogButton:
 	_choices.add_child(dialog_button)
 	return dialog_button
 
+func _get_fade_out_delay() -> float:
+	return _dice_fade_out_delay if is_dice_page() else _fade_out_delay
+
+func _set_state(new_state: State) -> void:
+	super._set_state(new_state)
+	match state:
+		State.PAST:
+			for dialog_button: DialogButton in _choices.get_children(): dialog_button.active = false
+		State.PRESENT:
+			for dialog_button: DialogButton in _choices.get_children(): dialog_button.active = true
+		_: assert(false, "StoryPageEntry.State %s is not supported!" % state)
+
 func _on_save_requested(save_request: SaveRequest, source: StoryDecision) -> void:
 	assert(save_request)
 	assert(source)
@@ -126,16 +86,3 @@ func _on_decision_made(_story_decision: StoryDecision, _selected_how_many_times:
 	_description.set_text_normally()
 	state = State.PAST
 	_story.decision_made.disconnect(_on_decision_made)
-
-func _on_mouse_entered() -> void:
-	assert(state == State.PAST)
-	if story_page.get_background(_story): _background.expand_mode = TextureRect.EXPAND_FIT_HEIGHT_PROPORTIONAL
-	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(_body_container, "modulate", Color.WHITE, _fade_in_duration)
-
-func _on_mouse_exited() -> void:
-	assert(state == State.PAST)
-	if get_global_rect().has_point(get_viewport().get_mouse_position()): return
-	_background.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
-	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(_body_container, "modulate", _past_modulate, _fade_in_duration)
