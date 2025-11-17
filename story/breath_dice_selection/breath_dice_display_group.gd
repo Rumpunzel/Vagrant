a2c613d2f79746e0d23fdc9484859bb17b707de1
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-11-17 14:08:25 +0100

    small fix

--- story/breath_dice_selection/breath_dice_display_group.gd
+++ story/breath_dice_selection/breath_dice_display_group.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	breath_die_button_added.connect(_on_breath_die_button_added)
 
 func _on_breath_die_button_added(breath_die_button: BreathDieButton) -> void:
-	var stack_color: Color = Color.WHITE * pow(0.5, get_elements().size())
+	var stack_color: Color = Color.WHITE * pow(0.5, get_elements().size() - 1)
 	stack_color.a = 1.0
 	breath_die_button.self_modulate = stack_color
 	_update_tooltip()
